44886c955ffd6b457f9a39de0d7ca24aadf9396c
ezeabregu <ezeabregu.ea@gmail.com>
2024-06-27 21:58:47 -0300

    prueba cors

--- models/server.ts
+++ models/server.ts
@@ -24,7 +24,7 @@ export class Server {
   }
   middlewares(): void {
     this.app.use(express.json());
-    this.app.use(cors());
+    this.app.use(cors({ origin: ["https://libros-page.vercel.app"] }));
   }
   routes(): void {
     this.app.use(this.authPath, authRoutes);
