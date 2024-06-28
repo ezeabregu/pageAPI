65d17cd9fa48397e1a258910b7485fb6a2fbcbb1
ezeabregu <ezeabregu.ea@gmail.com>
2024-06-27 22:48:01 -0300

    prueba cors

--- models/server.ts
+++ models/server.ts
@@ -24,7 +24,13 @@ export class Server {
   }
   middlewares(): void {
     this.app.use(express.json());
-    this.app.use(cors());
+    this.app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+      })
+    );
   }
   routes(): void {
     this.app.use(this.authPath, authRoutes);
