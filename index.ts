4ef8ec9de24ad6ad6f3f3488fe05e63f34b4139f
ezeabregu <ezeabregu.ea@gmail.com>
2024-06-29 01:00:07 -0300

    prueba api 1:00

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import { Server } from "./models/server";
 
 //const express = require("express");
 const app = express();
+
+dotenv.config();
+const server = new Server();
+server.listen();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
