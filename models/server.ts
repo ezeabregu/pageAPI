e36eb85d135cc5aab8bcc585a80eda37544fff68
ezeabregu <ezeabregu.ea@gmail.com>
2024-07-28 13:36:27 -0300

    server changes

--- models/server.ts
+++ models/server.ts
@@ -3,18 +3,21 @@ import { dbConnection } from "../database/config";
 import cors from "cors";
 import authRoutes from "../routes/auth";
 import ordersRoutes from "../routes/orders";
+import librosRoutes from "../routes/products";
 
 export class Server {
   app: Express;
   port: string | number | undefined;
   authPath: string;
   ordersPath: string;
+  librosPath: string;
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.authPath = "/auth";
     this.ordersPath = "/orders";
+    this.librosPath = "/libros";
     this.conectarDB();
     this.middlewares();
     this.routes();
@@ -29,6 +32,7 @@ export class Server {
   routes(): void {
     this.app.use(this.authPath, authRoutes);
     this.app.use(this.ordersPath, ordersRoutes);
+    this.app.use(this.librosPath, librosRoutes);
   }
   listen(): void {
     this.app.listen(this.port, () => {
